ba0d049beea99d23edd844dc58c630ca428e50a7
yzt <1327023855@qq.com>
2019-04-19 14:55:29 +0800

    this is a practice.

---
+++ practice.py
@@ -0,0 +1,7 @@
+my_list =["jake","Tom","xi li"]
+# for name in my_list:
+#     print(name)
+# for number in range(11):
+#     print(number)
+for letter in 'yuan'
+       print("each letter is:" + letter)
